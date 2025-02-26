12d87988dbe24289d2772141f52e0c002830a56e
jianxi <17637145793@163.com>
2025-02-26 09:29:12 +0800

    update model_test

--- src/python/model_test/LogisticRegression/预测信用卡违约情况/main.py
+++ src/python/model_test/LogisticRegression/预测信用卡违约情况/main.py
@@ -23,8 +23,19 @@ X_scaled = scaler.fit_transform(X)
 # 划分训练集和测试集
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
+from imblearn.over_sampling import SMOTE
+
+# 应用 SMOTE 过采样
+smote = SMOTE(sampling_strategy='auto', random_state=42)
+X_train, y_train = smote.fit_resample(X_train, y_train)
+
+# 打印过采样后的数据分布
+print(y_train.value_counts())
+
 # 训练逻辑回归模型
 model = LogisticRegression()
+# 使用class_weight='balanced'(设置权重)参数来处理类别不平衡问题
+model = LogisticRegression(class_weight = {0: 1, 1: 1.05})
 model.fit(X_train, y_train)
 
 # 预测与评估
@@ -50,8 +61,3 @@ plt.ylabel('Actual')
 plt.title('Confusion Matrix')
 plt.show()
 
-# 模型系数
-coefficients = model.coef_[0]
-feature_names = X.columns
-for feature, coef in zip(feature_names, coefficients):
-    print(f"{feature}: {coef:.4f}")
